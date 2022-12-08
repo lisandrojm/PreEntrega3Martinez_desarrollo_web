e6ee0f98c219da59f11dcf2cc384a17cb50fe509
lisandrojm <lisandrojm@gmail.com>
2022-12-08 17:30:24 -0300

    update

--- js/main.js
+++ js/main.js
@@ -18,10 +18,6 @@ up.onclick = function() {
   var header = document.getElementById("site-header");
 
   var checkScroll = function() {
-    /*
-     ** Find the direction of scroll
-     ** 0 - initial, 1 - up, 2 - down
-     */
 
     curScroll = w.scrollY || doc.scrollTop;
     if (curScroll > prevScroll) {
@@ -41,7 +37,6 @@ up.onclick = function() {
 
   var toggleHeader = function(direction, curScroll) {
     if (direction === 2 && curScroll > 52) {
-      //replace 52 with the height of your header in px
 
       header.classList.add("hide");
       prevDirection = direction;
